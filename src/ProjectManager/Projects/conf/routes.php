e1d4bca131b69fecd8e7b3cd4bc26cf605c35ffe
Iago Oliveira <silva.io@outlook.com>
2016-07-12 21:22:00 -0300

    Grouping routes for Projects module.

--- src/ProjectManager/Projects/conf/routes.php
+++ src/ProjectManager/Projects/conf/routes.php
@@ -3,59 +3,71 @@
 // Default route from scaffolding
 // Feel free to change it.
 
-$app->get('/Projects/Project/list',function() use ($app){
+$app->group('/Projects', function() use ($app){
+	
+	$app->group('/Project/', function() use ($app){
+	
+		$app->get('list',function() use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProjectController($app))->listProject();
+		
+		})->via('GET','POST')->name('Projects-Project-List');
+		
+		$app->get('create',function() use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProjectController($app))->createProject();
+		
+		})->via('GET','POST')->name('Projects-Project-Create');
+		
+		$app->get('edit/:id',function($id) use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProjectController($app))->editProject($id);
+		
+		})->via('GET','POST')->name('Projects-Project-Edit');
+		
+		$app->get('delete/:id',function($id) use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProjectController($app))->deleteProject($id);
+		
+		})->via('GET','POST')->name('Projects-Project-Delete');
+		
+	});
+	
+	$app->group('/Products/', function() use ($app){
+	
+		$app->get(':projectId',function($projectId) use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProductController($app))->listProduct($projectId);
+		
+		})->via('GET','POST')->name('Projects-Products-List');
+		
+		$app->get('create/:projectId',function($projectId) use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProductController($app))->createProduct($projectId);
+		
+		})->via('GET','POST')->name('Projects-Product-Create');
+		
+		$app->get('edit/:projectId/:id',function($projectId,$id) use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProductController($app))->editProduct($projectId, $id);
+		
+		})->via('GET','POST')->name('Projects-Product-Edit');
+		
+		$app->get('delete/:projectId/:id',function($projectId,$id) use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProductController($app))->deleteProduct($projectId, $id);
+		
+		})->via('GET','POST')->name('Projects-Product-Delete');
+		
+		$app->get('Overview/',function() use ($app){
+		
+			(new ProjectManager\Projects\Controller\ProductController($app))->productsOverview();
+		
+		})->via('GET','POST')->name('Projects-Products-Overview');
+		
+	});
+	
+});
 
-    (new ProjectManager\Projects\Controller\ProjectController($app))->listProject();
-
-})->via('GET','POST')->name('Projects-Project-List');
-
-$app->get('/Projects/Project/create',function() use ($app){
-
-    (new ProjectManager\Projects\Controller\ProjectController($app))->createProject();
-
-})->via('GET','POST')->name('Projects-Project-Create');
-
-$app->get('/Projects/Project/edit/:id',function($id) use ($app){
-
-    (new ProjectManager\Projects\Controller\ProjectController($app))->editProject($id);
-
-})->via('GET','POST')->name('Projects-Project-Edit');
-
-$app->get('/Projects/Project/delete/:id',function($id) use ($app){
-
-    (new ProjectManager\Projects\Controller\ProjectController($app))->deleteProject($id);
-
-})->via('GET','POST')->name('Projects-Project-Delete');
 
 
-// Products
-$app->get('/Projects/Products/:projectId',function($projectId) use ($app){
-
-    (new ProjectManager\Projects\Controller\ProductController($app))->listProduct($projectId);
-
-})->via('GET','POST')->name('Projects-Products-List');
-
-// Products
-$app->get('/Projects/Products/create/:projectId',function($projectId) use ($app){
-
-    (new ProjectManager\Projects\Controller\ProductController($app))->createProduct($projectId);
-
-})->via('GET','POST')->name('Projects-Product-Create');
-
-$app->get('/Projects/Products/edit/:projectId/:id',function($projectId,$id) use ($app){
-
-    (new ProjectManager\Projects\Controller\ProductController($app))->editProduct($projectId, $id);
-
-})->via('GET','POST')->name('Projects-Product-Edit');
-
-$app->get('/Projects/Products/delete/:projectId/:id',function($projectId,$id) use ($app){
-
-    (new ProjectManager\Projects\Controller\ProductController($app))->deleteProduct($projectId, $id);
-
-})->via('GET','POST')->name('Projects-Product-Delete');
-
-$app->get('/Projects/Products/Overview/',function() use ($app){
-
-    (new ProjectManager\Projects\Controller\ProductController($app))->productsOverview();
-
-})->via('GET','POST')->name('Projects-Products-Overview');
